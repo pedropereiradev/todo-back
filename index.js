6421eabd3c062dc73bb391ca83ef97e73b497cf6
Pedro Pereira <pedro.2108@hotmail.com>
2023-01-09 20:32:37 -0300

    initial project

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = 3001
+
+const database = [];
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json(database);
+})
+
+app.post('/', (req, res) => {
+  const task = req.body.todo;
+  database.push(task);
+
+  res.status(201).json(database);
+})
+
+app.delete('/', (req, res) => {
+  const { task } = req.body;
+
+  const taskIndex = database.indexOf(task);
+
+  database.splice(taskIndex, 1);
+
+  res.status(200).json(database);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
